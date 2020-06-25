5653041f4062467b9494da822f6262fa1b210cea
Charles Floeder <cfloeder@google.com>
2020-06-25 13:36:51 +0000

    Revert "fix syntax error and update class comment"
    This reverts commit ed2c80697a7d0814d1004941c5bb64d093a02c33.

--- src/java/data/Account.java
+++ src/java/data/Account.java
@@ -14,7 +14,7 @@
 package com.google.data;
 
 /**
- * A data container for a billing account object.
+ * A class representing a billing account object.
  */
 public class Account {
     private final String accountID;
