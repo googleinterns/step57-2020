de8a2f43d922a2608dc49a47464b710d5b759148
Charles Floeder <cfloeder@google.com>
2020-06-26 17:42:11 +0000

    create SheetsAuthUtil class to create credentials for Sheets API interaction

---
+++ src/main/java/data/SheetsAuthUtil.java
@@ -0,0 +1,43 @@
+package data;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.MemoryDataStoreFactory;
+import com.google.api.services.sheets.v4.SheetsScopes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+/** 
+ * A class to create and send a Credential object that allows for Sheets API interaction 
+ */
+public class SheetsAuthUtil {
+    public static Credential authorize() throws IOException, GeneralSecurityException {
+         
+        // Build GoogleClientSecrets from JSON file.
+        InputStream in = GoogleAuthorizeUtil.class.getResourceAsStream("/google-sheets-api.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new InputStreamReader(in));
+
+        List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
+ 
+        // Build Credential object.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+            GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), 
+            clientSecrets, scopes).setDataStoreFactory(new MemoryDataStoreFactory()).
+            setAccessType("offline").build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+
+        return credential;
+    }
+}
+
