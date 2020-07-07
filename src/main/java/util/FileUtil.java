f753ae06e20189d6f97b0b394bf394c4a7f8c93a
Cade Crandall <cadecrandall@google.com>
2020-07-07 16:02:31 +0000

    Add dummy return values

---
+++ src/main/java/util/FileUtil.java
@@ -0,0 +1,26 @@
+package util;
+
+import data.Vendor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileUtil {
+  public static ArrayList<String> getVendorIDs() {
+    return new ArrayList<>(Arrays.asList("ALPHA", "BRAVO", "CHARLIE", "DELTA"));
+  }
+
+  public static ArrayList<String> getAccountIDs(String vendorID) {
+    return new ArrayList<>(Arrays.asList("ECHO", "FOXTROT", "GOLF", "HOTEL"));
+  }
+
+  /** makes changes to the file in the codebase */
+  public static void updateFile(Vendor vendor) {
+
+  }
+
+  /** return true when delete succeeds? */
+  public static boolean deleteVendorConfig(String vendorID) {
+
+  }
+}
