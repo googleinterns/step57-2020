306899d177f4eb719271bfe399006d5fc9271f84
Charles Floeder <cfloeder@google.com>
2020-07-07 13:10:30 +0000

    add toJson() method to the Account class

--- src/main/java/data/Account.java
+++ src/main/java/data/Account.java
@@ -13,6 +13,9 @@
 // limitations under the License.
 package data;
 
+import java.util.*;
+import org.json.JSONObject;
+
 /** A class representing a billing account object. */
 public class Account {
   private String accountID;
@@ -110,5 +113,33 @@ public class Account {
 
   public void setAggregationMode(String aggregationMode) {
     this.aggregationMode = aggregationMode;
+  }
+
+  // Create and return a JSON object that contains Account fields.
+  public LinkedHashMap toJson() {
+    // Use LinkedHashMap to preserve order.
+    LinkedHashMap accountMap = new LinkedHashMap();
+    LinkedHashMap settlementAttributesMap = new LinkedHashMap();
+
+    // Create a JSON objects to contain Account fields.
+    JSONObject settlementConfig = new JSONObject();
+
+    // Populate settlementConfig JSON object. LEARN HOW TO PUT QUOTES AROUND A VARIABLE
+    settlementConfig.put("Matching_Mode", getMatchingMode());
+
+    // Populate settlementAttributes JSON object.
+    settlementAttributesMap.put("Currency_Code", getCurrency());
+    settlementAttributesMap.put("Direction", getDirection());
+    settlementAttributesMap.put("Entity", getEntity());
+
+    // Populate JSON account object.
+    accountMap.put("Legacy_Account_ID", getLegacyAccountID());
+    accountMap.put("Next_Gen_Customer_ID", getNextGenAccountID());
+    accountMap.put("Settlement_Attributes", settlementAttributesMap);
+    accountMap.put("Settlement_Config", settlementConfig);
+    accountMap.put("Account_ID", getAccountID());
+    accountMap.put("Aggregation_Mode", getAggregationMode());
+
+    return accountMap;
   }
 }
