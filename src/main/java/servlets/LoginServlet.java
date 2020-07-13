90478c6bf23fd80b0aafe6d78276634af36281ed
Charles Floeder <cfloeder@google.com>
2020-07-13 18:00:19 +0000

    add OAuthConstants class to hold constants used in Login and OAuthCallback servlets

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -24,19 +24,16 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import util.UserAuthUtil;
+import util.OAuthConstants;
 
 @WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
   private static final Logger LOGGER = Logger.getLogger(LoginServlet.class.getName());
   private static final String CONTENT_TYPE_TEXT_HTML = "text/html;";
   private static final String REDIRECT_LINK = "/Login";
-  private final String CLIENT_ID = "client_id=150737768611-svndjtlklolq53g4ass4r3sqal2i31p5.apps.googleusercontent.com";
   private final String SCOPE = "scope=https://www.googleapis.com/auth/spreadsheets";
-  private final String REDIRECT_URI = "redirect_uri=";
   private final String OAUTH_LOGIN_URI = "https://accounts.google.com/o/oauth2/v2/auth";
   private final String RESPONSE_TYPE = "response_type=code";
-  private final String OAUTH_CALLBACK_SERVLET = "/api/oauth/callback/sheets";
-  private final String ENVIRONMENT_VARIABLE = "DOMAIN";
 
   // Returns a URL to either login and get OAuth tokens or to logout.
   @Override
@@ -53,17 +50,17 @@ public class LoginServlet extends HttpServlet {
 
   /** Build the OAuth consent page redirect URL. */
   private String getOAuthRedirectURL() {
-    String redirectUri = REDIRECT_URI + getRedirectUri();
-    
-    return String.format("%s?%s&%s&%s&%s", OAUTH_LOGIN_URI, CLIENT_ID,
-      RESPONSE_TYPE, redirectUri, SCOPE); 
+    return String.format("%s?%s&%s&%s&%s", OAUTH_LOGIN_URI, 
+      OAuthConstants.CLIENT_ID, RESPONSE_TYPE, getRedirectUri(), SCOPE); 
   }
 
   // Build a valid redirect URI to the OAuthCallbackServlet.
   private String getRedirectUri() {
     try {
-      URI domainUri = URI.create(System.getenv().get(ENVIRONMENT_VARIABLE));
-      return domainUri.resolve(OAUTH_CALLBACK_SERVLET).toString();
+      URI domainUri = URI.create(System.getenv().get(
+        OAuthConstants.ENVIRONMENT_VARIABLE));
+      return OAuthConstants.REDIRECT_URI + domainUri.resolve(
+        OAuthConstants.OAUTH_CALLBACK_SERVLET).toString();
     } catch (NullPointerException e) {
       LOGGER.severe("The DOMAIN environment variable is not set.");
       throw e;
--- src/main/java/servlets/OAuthCallbackServlet.java
+++ src/main/java/servlets/OAuthCallbackServlet.java
@@ -31,28 +31,22 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonElement;
+import util.OAuthConstants;
 
-/** A servlet to request access and refresh tokens. */
+/** A servlet to request access and refresh access tokens for the Sheets API.*/
 @WebServlet("/api/oauth/callback/sheets")
 public class OAuthCallbackServlet extends HttpServlet {
   private static final Logger LOGGER = Logger.getLogger(LoginServlet.class.getName());
-  private final String CLIENT_ID = "client_id=150737768611-svndjtlklolq53g4ass4r3sqal2i31p5.apps.googleusercontent.com";
   private final String GRANT_TYPE = "grant_type=authorization_code";
   private final String TOKEN_URI = "https://oauth2.googleapis.com/token";
-  private final String CODE = "code=";
+  private final String AUTH_CODE = "code=";
   private final String CLIENT_SECRET = "client_secret=";
-  private final String REDIRECT_URI = "redirect_uri=";
   private final String SECRET_FILEPATH = "../../src/main/resources/secret.txt";
-  private final String OAUTH_CALLBACK_SERVLET = "/api/oauth/callback/sheets";
-  private final String ENVIRONMENT_VARIABLE = "DOMAIN";
-
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String error = request.getParameter("error");
-    String code = request.getParameter("code");
-
-    System.out.println("CODE: "+ code);
+    String authCode = request.getParameter("code");
 
     // Print any error the OAuth provider gave us.
     if (error != null && !error.isEmpty()) {
@@ -61,26 +55,29 @@ public class OAuthCallbackServlet extends HttpServlet {
       return;
     }
 
-    // OAuth provider should have given us a code, otherwise something is wrong here
-    if (code == null || code.isEmpty()) {
+    // Check that the OAuth provider gave us the authorization code.
+    if (authCode == null || authCode.isEmpty()) {
       response.setStatus(400);
       return;
     }
 
-    String tokenRequestBody = String.format("%s&%s&%s&%s&%s", GRANT_TYPE, CODE + code,
-      getRedirectUri(), CLIENT_ID, getClientSecret());
+    String tokenRequestBody = String.format("%s&%s&%s&%s&%s", GRANT_TYPE, 
+      AUTH_CODE + authCode, getRedirectUri(), OAuthConstants.CLIENT_ID, 
+      getClientSecret());
 
-    // Request the tokens.
+    // Request the access tokens.
     HttpClient httpClient = HttpClient.newHttpClient();
     HttpRequest tokenRequest = HttpRequest.newBuilder(URI.create(TOKEN_URI))
       .header("Content-Type", "application/x-www-form-urlencoded")
       .POST(BodyPublishers.ofString(tokenRequestBody)).build();
 
-    HttpResponse tokenResponse = httpClient.sendAsync(tokenRequest, BodyHandlers.ofString()).join();
+    HttpResponse tokenResponse = httpClient.sendAsync(tokenRequest, 
+      BodyHandlers.ofString()).join();
     String tokenResponseBody = tokenResponse.body().toString();
 
     // Parses to find access token.
-    JsonObject tokenResponseObj = JsonParser.parseString(tokenResponseBody).getAsJsonObject();
+    JsonObject tokenResponseObj = JsonParser.parseString(tokenResponseBody)
+      .getAsJsonObject();
     JsonElement accessToken = tokenResponseObj.get("access_token");
 
     response.setContentType("text/html");
@@ -90,8 +87,10 @@ public class OAuthCallbackServlet extends HttpServlet {
   // Build a valid redirect URI to the OAuthCallbackServlet.
   private String getRedirectUri() {
     try {
-      URI domainUri = URI.create(System.getenv().get(ENVIRONMENT_VARIABLE));
-      return REDIRECT_URI + domainUri.resolve(OAUTH_CALLBACK_SERVLET).toString();
+      URI domainUri = URI.create(System.getenv().get(
+        OAuthConstants.ENVIRONMENT_VARIABLE));
+      return OAuthConstants.REDIRECT_URI + domainUri.resolve(
+        OAuthConstants.OAUTH_CALLBACK_SERVLET).toString();
     } catch (NullPointerException e) {
       LOGGER.severe("The DOMAIN environment variable is not set.");
       throw e;
