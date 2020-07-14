9d1ef9d55d430ef251457be8602e600bf9696351
Cade Crandall <cadecrandall@google.com>
2020-07-14 21:57:01 +0000

    add javadoc

--- src/main/java/data/ResponseBuilder.java
+++ src/main/java/data/ResponseBuilder.java
@@ -2,10 +2,16 @@ package data;
 
 import util.UserAuthUtil;
 
+/** Class to build Login/Logout response objects in JSON format */
 public class ResponseBuilder {
+  /**
+   * @param isLoggedIn user login status
+   * @param redirectURL URL to redirect after login/logout
+   * @return JSON string with login/logout URL
+   *         e.g., logged in: { "isLoggedIn":{"/_ah/logout?continue=%2FLogin"}, "isLoggedOut":{""}}
+   *         logged out: { "isLoggedIn":{""}, "isLoggedOut":{"/_ah/login?continue=%2FLogin"}}
+   */
   public static String toJson(boolean isLoggedIn, String redirectURL) {
-    // logged in: { "isLoggedIn":{"/_ah/logout?continue=%2FLogin"}, "isLoggedOut":{""}}
-    // logged out: { "isLoggedIn":{""}, "isLoggedOut":{"/_ah/login?continue=%2FLogin"}}
     return "{ \"isLoggedIn\":{\"" + buildLogout(isLoggedIn, redirectURL) + "\"}, " +
             "\"isLoggedOut\":{\"" + buildLogin(isLoggedIn, redirectURL) + "\"}}";
   }
