375f49af2f3ba182f933592be8ef1069c6ae0d69
Vincent Stevenson <vincentjr34@yahoo.com>
2020-07-15 21:27:43 +0000

    Adding function to display json configuration to readfile. Minor revisions to populateCustomerList().

--- src/main/webapp/appscript.js
+++ src/main/webapp/appscript.js
@@ -13,27 +13,49 @@
 // limitations under the License.
 
 /**
-  * Fetches vendor IDs from VendorServlet and adds them to the
+  * Fetch vendor IDs from VendorServlet and add them to the
   * Customer ID dropdown.
   */
 async function populateCustomerList() {
   const response = await fetch('/VendorServlet');
 
+  // VendorServlet returns a hashmap/JSON dictionary. 
+
   // Response is a json array that is parsed into json.
-  var customer_json = await response.json();
+  var customerJson = await response.json();
 
   // Loops over this array to make the options for the Customer ID dropdown.
   var html = '';
-  for (var i = 0; i < customer_json.length; i++) {
-    html += '<option value="' + customer_json[i] + '">'
-    + customer_json[i] + '</option>';
+  for (var i = 0; i < customerJson.length; i++) {
+    html += '<option value="' + customerJson[i] + '">'
+    + customerJson[i] + '</option>';
   }
 
   // Adds the options to the page, allowing them to be viewed. 
   document.getElementById('customer-ids').innerHTML = html;
 }
 
-// TODO: Fetch method for putting the specified config to the page.
+/**
+ * Fetch a json configuration as a string and formats it to be shown on
+ * the readfile page.
+ */
+async function addToPage() {
+  var vendorID = document.getElementById('customer-ids');
+  var accountID = document.getElementById('account-ids');
+
+  const response = await fetch("/BillingConfig?" + "vendor-id=" 
+    + vendorID + "&account-id=" + accountID);
+
+  // Response is a JSON as a string.
+  let config_json = await response.json();
+
+  var html = document.getElementById('json-box');
+
+  // Stringify converts the json into a string.
+  html.innerHTML = JSON.stringify(config_json, undefined, 4);
+
+  // TODO: Add various ways to display the json.
+}
 // TODO: Method to populate edit form.
 // TODO: Method for enums: Check which of the tags has data under it {isLoggedIn:{loginURL},isLoggedOut:{logooutURL}}}
 
