e9f1aac4bd0481cc5f35daf98d5823f36ce61708
Charles Floeder <cfloeder@google.com>
2020-07-31 13:14:35 +0000

    replace String literals with constants

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -29,27 +29,25 @@ public class LoginServlet extends HttpServlet {
   private static final String OAUTH_URL = "/OAuth";
   private static final String LOG_PAGE_URL = "/login.html";
   private static final String TEXT_TYPE = "text/html";
+  private final String LOGOUT_URL = "logoutURL";
+  private final String LOGIN_URL = "loginURL";
+  private final String LOGIN_STATUS = "loginStatus";
+  private final String TRUE = "true";
+  private final String FALSE = "false";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType(TEXT_TYPE);
     UserService userService = UserServiceFactory.getUserService();
     HashMap<String, String> loginObject = new HashMap<String, String>();
-    String loginURL = "";
-    String logoutURL = "";
-    String loginStatus = "";
 
     if(userService.isUserLoggedIn()) {
-      logoutURL = userService.createLogoutURL(LOG_PAGE_URL); 
-      loginStatus = "true"; 
+      loginObject.put(LOGOUT_URL, userService.createLogoutURL(LOG_PAGE_URL)); 
+      loginObject.put(LOGIN_STATUS, TRUE); 
     } else {
-      loginURL = userService.createLoginURL(OAUTH_URL);
-      loginStatus = "false";
+      loginObject.put(LOGIN_URL, userService.createLoginURL(OAUTH_URL));
+      loginObject.put(LOGIN_STATUS, FALSE); 
     }
-
-    loginObject.put("loginURL", loginURL);
-    loginObject.put("logoutURL", logoutURL);
-    loginObject.put("loginStatus", loginStatus);
 
     // Convert the URL to a JSON String.
     String jsonMessage = messageListAsJson(loginObject);
